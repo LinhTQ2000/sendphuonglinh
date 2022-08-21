33068584521a5389aabc1c36aff62dfc4c4bfa83
linh.taquang <linh.taquang@vti.com.vn>
2022-08-21 11:59:10 +0700

    update intro

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
 const CONFIG = {
   introTitle: "Babe à!",
-  introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+  introDesc: `Mong em chân cứng đá mềm, đường xa không mỏi, đường đời có anh ! 😚💙`,
   btnIntro: "hihi",
   title: "Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙",
   desc: "Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ",
